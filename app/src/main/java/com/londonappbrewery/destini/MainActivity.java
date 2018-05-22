d15177d3d31a317a95e51bdec3bdbc20b273ece5
sandipan94 <sandipan.bera6@gmail.com>
2018-05-23 01:06:05 +0530

    challenge complete

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,20 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    TextView mStoryTextView;
+
+    Button mButtonTop;
+    Button mButtonBottom;
+    boolean mButtonTopPressed;
+    int mIndex=1;
+
 
 
     @Override
@@ -15,16 +25,73 @@ public class MainActivity extends AppCompatActivity {
 
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
+        mStoryTextView=(TextView)findViewById(R.id.storyTextView);
+        mButtonTop=(Button)findViewById(R.id.buttonTop);
+        mButtonBottom=(Button)findViewById(R.id.buttonBottom);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
 
+        mButtonTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mButtonTopPressed=true;
+                nextStory();
 
+            }
+        });
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mButtonBottom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextStory();
 
+            }
+        });
 
+    }
+    private void nextStory(){
+
+        switch (mIndex){
+            case 1:
+                if(mButtonTopPressed){
+                    mIndex = 3;
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mButtonTop.setText(R.string.T3_Ans1);
+                    mButtonBottom.setText(R.string.T3_Ans2);
+                    break;
+                }
+                mIndex = 2;
+                mStoryTextView.setText(R.string.T2_Story);
+                mButtonTop.setText(R.string.T2_Ans1);
+                mButtonBottom.setText(R.string.T2_Ans2);
+                break;
+            case 2:
+                if(mButtonTopPressed){
+                    mIndex = 3;
+                    mStoryTextView.setText(R.string.T3_Story);
+                    mButtonTop.setText(R.string.T3_Ans1);
+                    mButtonBottom.setText(R.string.T3_Ans2);
+                    break;
+                }
+                mStoryTextView.setText(R.string.T4_End);
+                mButtonTop.setVisibility(View.GONE);
+                mButtonBottom.setVisibility(View.GONE);
+                break;
+            case 3:
+                if(mButtonTopPressed){
+                    mStoryTextView.setText(R.string.T6_End);
+                    mButtonTop.setVisibility(View.GONE);
+                    mButtonBottom.setVisibility(View.GONE);
+                    break;
+                }
+                mStoryTextView.setText(R.string.T5_End);
+                mButtonTop.setVisibility(View.GONE);
+                mButtonBottom.setVisibility(View.GONE);
+                break;
+        }
+        mButtonTopPressed = false;
     }
 }
